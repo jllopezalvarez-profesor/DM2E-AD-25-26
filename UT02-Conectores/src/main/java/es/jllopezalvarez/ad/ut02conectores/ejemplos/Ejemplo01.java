e6ea77d8e844a98161cb9060a025527bb34736ab
José Luis López Álvarez <jllopezalvarez@educa.madrid.org>
2025-10-30 21:05:38 +0100

    Ejemplo acceso a BD JDBC

---
+++ UT02-Conectores/src/main/java/es/jllopezalvarez/ad/ut02conectores/ejemplos/Ejemplo01.java
@@ -0,0 +1,31 @@
+package es.jllopezalvarez.ad.ut02conectores.ejemplos;
+
+import java.sql.*;
+
+public class Ejemplo01 {
+
+    public static void main(String[] args) {
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/chinook", "chinook", "chinook")) {
+            System.out.printf("Connected to database at: %s\n", connection.getMetaData().getURL());
+            System.out.printf("El tipo del objeto connection es %s\n", connection.getClass().getCanonicalName());
+            try (Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery("select  name, genre_id from genre")) {
+
+                if (!resultSet.isBeforeFirst()) {
+                    System.out.println("No hay resultados");
+                } else {
+                    System.out.println("Géneros encontrados:");
+                    while (resultSet.next()) {
+                        //System.out.printf("%s - %s \n",  resultSet.getString(1), resultSet.getString(2) );
+                        System.out.printf("%s - %s \n", resultSet.getString("genre_id"), resultSet.getString("name"));
+                    }
+                }
+            }
+
+        } catch (SQLException e) {
+            System.out.printf("Error connecting to database at: %s\n", e.getMessage());
+        }
+    }
+
+
+}
